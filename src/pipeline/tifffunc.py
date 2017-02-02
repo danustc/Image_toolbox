81dcdbaea4fce719279801a2a2e06c16a51aee95
danustc <xdlaway@gmail.com>
2017-02-02 13:44:46 -0800

    0202_Rock-1

--- src/pipeline/tifffunc.py
+++ src/pipeline/tifffunc.py
@@ -1,5 +1,5 @@
 """
-A wrapper designed for Dan's image processing. 
+A wrapper designed for Dan's image processing.
 Based on Christoph Gohlke (UCI)'s tifffile module.
 Last modification: 08/15/16
 """
@@ -17,7 +17,7 @@ def read_tiff(fname):
 
 def intp_tiff(istack, ns1, ns2, nint = 1):
     # linear interpolation of slices between
-    int_stack = np.zeros(shape = (nint,)+ istack.shape[1:]) 
+    int_stack = np.zeros(shape = (nint,)+ istack.shape[1:])
     for ii in np.arange(nint + 2):
         alpha = ii/(nint + 1.)
         int_stack[ii] = istack[ns1]*(1-alpha) + istack[ns2]*alpha
@@ -26,4 +26,25 @@ def intp_tiff(istack, ns1, ns2, nint = 1):
 def write_tiff(imstack, fname):
     # assume that fname already has the extension.
     imsave(fname, imstack.astype('uint16'))
-    
+
+def reorient_tiff_RAS(imstack, fname):
+    '''
+    Reoriente the image stack in the RAS coordinate system.
+    '''
+    # nz, nx, ny = imstack.shape
+    rot_stack = []
+    for zslice in imstack:
+        rot_stack.append(np.rot90(zslice, k=3))
+
+    rot_stack = np.array(rot_stack)
+    write_tiff(rot_stack, fname)
+
+
+
+def main():
+    impath = '/home/sillycat/Documents/Zebrafish/Exp_figures/ZD_2.tif'
+    tiff = read_tiff(impath)
+    reorient_tiff_RAS(tiff, impath[:-4]+ '_rio.tif')
+
+if __name__ == '__main__':
+    main()
