b8453af26eb84d6261489b470073415308f9235b
danustc <xdlaway@gmail.com>
2017-05-02 23:14:45 -0700

    0502_Home-3

--- src/visualization/stat_present.py
+++ src/visualization/stat_present.py
@@ -6,7 +6,6 @@ import sys
 sys.path.append('/home/sillycat/Programming/Python/Image_toolbox/')
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
 from mpl_toolkits.mplot3d import Axes3D
 
 
@@ -19,46 +18,31 @@ def PCA_scatter_matrix(pc_data, dim_select = None):
         dim_select = np.arange(np.max(3, pc_data.shape[1]))
 
     ndim = len(dim_select) # the dimension of dim_select
-    fig_pc = plt.figure(figsize = (2*ndim, 2*ndim))
-    grid_layout = gridspec.GridSpec(ndim, ndim, wspace = 0.0, hspace = 0.0)
+    fig_pc, axes = plt.subplots(nrows = ndim, ncols = ndim, figsize = (2*ndim, 2*ndim))
+    fig_pc.subplots_adjust(hspace = 0.05, wspace = 0.05)
     for nr in range(ndim):
         #iterate over rows
         pc_row = pc_data[:,dim_select[nr]]
-        tx = plt.subplot(grid_layout[nr,nr]) # the diagonal blocks
+        tx = axes[nr,nr]
+        tx.xaxis.set_visible(False)
+        tx.yaxis.set_visible(False)
         tx.annotate('PC '+ str(nr), (0.5, 0.5),xycoords = 'axes fraction', ha = 'center', va = 'center', fontsize = 14)
         for nc in np.arange(nr):
             # iterate over columns
             pc_col= pc_data[:,dim_select[nc]]
-            ax = plt.subplot(grid_layout[nr, nc])
-            ax.scatter(pc_row,pc_col, c = 'b', s = 5, cmap = plt.cm.spectral)
+            ax = axes[nr,nc]
             ax.xaxis.set_visible(False)
             ax.yaxis.set_visible(False)
-            if () 
+            ax.scatter(pc_row,pc_col, c = 'b', s = 5, cmap = plt.cm.spectral)
+            if ax.is_first_col():
+                ax.yaxis.set_visible(True)
                 ax.yaxis.set_ticks_position('left')
-            if ax.is_last_col():
-                ax.yaxis.set_ticks_position('right')
-            if ax.is_first_row():
-                ax.xaxis.set_ticks_position('top')
-            if ax.is_last_row():
-                ax.xaxis.set_ticks_position('bottom')
-
 
             # plot the transposed half
-            ax = plt.subplot(grid_layout[nc, nr])
-            ax.scatter(pc_col, pc_row, c = 'b', s = 5, cmap = plt.cm.spectral)
+            ax = axes[nc,nr]
             ax.xaxis.set_visible(False)
             ax.yaxis.set_visible(False)
-            if ax.is_first_col():
-                ax.yaxis.set_ticks_position('left')
-            if ax.is_last_col():
-                ax.yaxis.set_ticks_position('right')
-            if ax.is_first_row():
-                ax.xaxis.set_ticks_position('top')
-            if ax.is_last_row():
-                ax.xaxis.set_ticks_position('bottom')
-
-
-
+            ax.scatter(pc_col, pc_row, c = 'b', s = 5, cmap = plt.cm.spectral)
 
         # two-dimensional plot 
 
