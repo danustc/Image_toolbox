72274144fd762faf3e0da2d7e60e2db78f345aec
danustc <xdlaway@gmail.com>
2017-06-13 11:57:27 -0700

    0613_Rock01

--- src/preprocessing/tifffunc.py
+++ src/preprocessing/tifffunc.py
@@ -9,26 +9,20 @@ import numpy as np
 
 
 # read a tiff stack
-def tiff_describe(fname):
+def tiff_describe(fname, handle_open = True):
     '''
     describe the properties of the tiff stack: number of slices, y,x pixels
     '''
     with TiffFile(fname) as tif:
         stack_size = tif.fstat.st_size/(1024*1024)
-        stack_info = tif.is_imagej.split() # imageJ description of the stack, in bytes instead of str
-        for inf_line in stack_info:
-            print(inf_line)
-            if b'slices=' in inf_line:
-                slice_number= inf_line.split(b'=')[1]
-                break
-    tif.close() # remember to close the tif to save the memory.
-    return stack_size, slice_number
+        stack_shape = tif.series[0].shape # imageJ description of the stack, in bytes instead of str
 
+    if handle_open:
+        return stack_size, stack_shape, tif
+    else:
+        tif.close() # remember to close the tif to save the memory.
+        return stack_size,stack_shape
 
-def tiff_handlepass(fname):
-    '''
-    for large tiff stacks, open a TiffFile class and pass the tif handle to the user. Note that the tif handle is open, so please close it after use!
-    '''
 
 def read_tiff(fname, nslice = None):
     # the fname should include the absolute path and extension name
