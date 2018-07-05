72e055c9354571428d3d2c9346e55f958f11909d
danustc <xdlaway@gmail.com>
2018-07-05 11:46:51 -0700

    0705_Rock-1

--- src/preprocessing/crossalign_pipeline.py
+++ src/preprocessing/crossalign_pipeline.py
@@ -12,8 +12,8 @@ import glob
 from src.preprocessing import affine as Affine
 from src.visualization.brain_navigation import slice_display,stack_display
 from src.preprocessing.red_detect import redund_detect_merge
-global_datapath = r"D:\/Data/2018-05-07\\"
-#global_datapath = r"X:\/Zebrafish_ispim/2018-04-16\\"
+#global_datapath = r"D:\/Data/2018-05-07\\"
+global_datapath = r"X:\/Zebrafish_ispim/2017-07-19\\"
 #global_datapath = '/home/sillycat/Programming/Python/data_test/'
 
 
@@ -190,7 +190,7 @@ def data_integrate(afc_merge, fluo_merge, rpixel = 0.295):
 def main():
     #relative_path = 'Nov01_2016_A1/'
     #full_path = global_datapath + relative_path
-    folder_list = glob.glob(global_datapath+'May*\\')
+    folder_list = glob.glob(global_datapath+'Jul*\\')
     for folder in folder_list:
         folder_date = os.path.basename(os.path.normpath(folder))
         print(folder_date)
