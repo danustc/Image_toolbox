4a1c1437849e4962e6f403b3b8398fd033aa244b
danustc <xdlaway@gmail.com>
2018-08-02 13:58:00 -0700

    0802_Rock-1

--- src/visualization/signal_plot.py
+++ src/visualization/signal_plot.py
@@ -3,6 +3,7 @@ Signal plot for \Delta F/F. Created by Dan on 07/27/2017.
 Last modification:
 '''
 import matplotlib.pyplot as plt
+from matplotlib import gridspec
 import numpy as np
 
 def compact_dffplot(dff_data, dt = 0.5, ts_bar = 200, sc_bar = 0.25):
@@ -80,7 +81,7 @@ def dff_rasterplot(dff_ordered, dt = 0.5, fw = 7.0, tunit = 'm', n_truncate = No
         fig = plt.figure(figsize = (fw, fw*5*canvas_scale*n_display/NT))
 
     ax = fig.add_subplot(111)
-    rshow = ax.imshow(dff_ordered[:, :n_display].T, cmap = 'Greens', interpolation = 'None', extent = [0., t_max, cell_tick[-1], cell_tick[0]], aspect = 'auto')
+    rshow = ax.imshow(dff_ordered[:, :n_display].T, cmap = 'plasma', interpolation = 'None', extent = [0., t_max, cell_tick[-1], cell_tick[0]], aspect = 'auto')
     ax.set_xticks(time_tick)
     ax.set_yticks(cell_tick)
     ax.set_yticklabels(cell_tick_label)
@@ -90,6 +91,40 @@ def dff_rasterplot(dff_ordered, dt = 0.5, fw = 7.0, tunit = 'm', n_truncate = No
     cbar.ax.tick_params(labelsize = 14)
     if title is not None:
         ax.set_title(title)
+
+    plt.tight_layout()
+    return fig
+
+
+def spectrogram(sg_gram, trange, krange, k_int = None):
+    '''
+    plot a spectrogram
+    if t_int, plot a time integration of the spectrogram
+    if k_int is None, do not plot the integration of spectrogram within the range of k_int.
+    '''
+    fig_width = 7.
+    NK, NW = sg_gram.shape
+    dk = krange/NK # the k_steps
+    fig = plt.figure(fig_size = (fig_width, fig_width*NK/NW))
+    sg_temp = (sg_gram**2).sum(axis = 1) # sum across time
+
+    if k_int is None:
+        gs = gridspec.GridSpec(1,2, width_ratios = [1,5])
+        ax_temp = fig_width.add_subplot(gs[0])
+        ax_sg = fig_width.add_subplot(gs[1]) # The imshow of spectrogram
+    else:
+        ki_, kf_ = int(k_int[0]/dk), int(np.ceil(k_int[1]/dk))
+        sg_int = (sg_gram[ki_:kf_]**2).sum(axis = 0)
+        gs = gridspec.GridSpec(2,2, width_ratios = [1,5], height_ratios = [1,4])
+        ax_int = fig_width.add_subplot(gs[0,1]) # The k_int plot
+        ax_temp = fig_width.add_subplot(gs[1,0]) # The k-space sum up
+        ax_sg = fig_width.add_subplot(gs[1,1]) # The imshow of spectrogram
+        ax_int.plot(sg_int, '-k')
+        ax_int.set_xticklabels([])
+
+    ax_sg.imshow(sg_gram[1:], origin = 'lower', aspect = 'auto', extent = [0, trange, dk, krange]) # show the non-zero frequency components
+    ax_temp.plot(sg_temp[1:], np.arange(1,NK)*dk, 'k')
+    ax_temp.set_xticklabels([])
 
     plt.tight_layout()
     return fig
