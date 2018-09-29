452ed8b1648599de5c3991c5cb81f55b5294fa1d
danustc <xdlaway@gmail.com>
2018-09-29 16:47:00 -0700

    0929_Home-1

--- src/analysis/group_analysis.py
+++ src/analysis/group_analysis.py
@@ -8,9 +8,10 @@ import os
 import glob
 from collections import deque
 
-global_datapath_ubn = '/home/sillycat/Programming/Python/data_test/'
+global_datapath_ubn = '/home/sillycat/Programming/Python/data_test/FB_resting_15min/'
 global_datapath_portable= '/media/sillycat/DanData/'
 
+dp_list = ['Jul2017/', 'Aug2018']
 
 class mass_grinder(object):
     '''
@@ -21,10 +22,11 @@ class mass_grinder(object):
         '''
         initialize the class, check the workpath
         '''
-        self._parse_folder_(work_path, name_flag)
         self.n_fish = 0 # the number of fish
         self.keys = deque()
         self.grinder_arr = deque()
+
+        self._parse_folder_(work_path, name_flag)
 
     def _parse_folder_(self, work_path, nf):
         '''
@@ -42,7 +44,24 @@ class mass_grinder(object):
                     self.keys.append(fish_key)
                     self.grinder_arr.append(fish_grinder)
                     self.n_fish +=1
-i                   print("Added fish:", fish_key)
+                    print("Added fish:", fish_key)
+
+    def select_fish(self, NF, fish_key):
+        pass
+
+
+    def anatomical_mask_statistics(self):
+        '''
+        Make a statistics for the masks covered in the group.
+        '''
+        nanno = 0
+        group_mask = np.array([])
+        for g in self.grinder_arr:
+            if g.annotated:
+                group_mask = np.union1d(group_mask, g.keys)
+                nanno +=1
+
+        ngm = group_mask.size # The total number of mask covered
 
 
 
@@ -50,4 +69,16 @@ i                   print("Added fish:", fish_key)
         '''
         clean up the class of the old data.
         '''
+        self.keys = deque()
+        self.grinder_arr = deque()
 
+
+# ----------------------------Below is a test main function. -------------
+
+def main():
+    path_Jul2017 = global_datapath_ubn + dp_list[0]
+    MG = mass_grinder(path_Jul2017)
+
+
+if __name__ == '__main__':
+    main()
