627dd5f8c3a38137a377749eea8a38f739a0b410
danustc <xdlaway@gmail.com>
2018-10-08 22:29:45 -0700

    1008_Home-0

--- src/preprocessing/drift_correction.py
+++ src/preprocessing/drift_correction.py
@@ -10,7 +10,6 @@ import correlation
 import patch_finding
 from PIL import Image, ImageSequence, ImageStat
 import glob
-import matplotlib.pyplot as plt
 import tifffile as tf
 import psutil
 from src.visualization import brain_navigation
@@ -20,6 +19,8 @@ global_datapath_win  = 'D:/Data/2018-08-23/B2_TS/\\'
 global_datapath_ubn = '/home/sillycat/Programming/Python/data_test/Image_labs/'
 global_datapath_ptb = '/media/sillycat/DanData/Jul19_2017_A2/A2_TS/'
 
+
+# ----------------- Here are some preparation functions----------------
 
 def stack_crop(raw_stack_path, patch_size = (512,512), seek_mode = 'center'):
     '''
@@ -31,11 +32,25 @@ def stack_crop(raw_stack_path, patch_size = (512,512), seek_mode = 'center'):
     '''
     # 1. take the first slice and calculate patch
     raw_dataset = Image.open(raw_stack_path)
-    slice_0 = raw_dataset.seek(0)
     w, h = raw_dataset.size # width and height
+    print(w,h)
     pw, ph = patch_size
-    ileft = int((w-pw)//2)
-    iupper = int((h-ph)//2)
+
+    if seek_mode == 'center':
+        # just crop the center patch from the original image
+        ileft = int((w-pw)//2)
+        iupper = int((h-ph)//2)
+
+    else:
+        # find the optimal patch
+        raw_dataset.seek(0)
+        slice_0 = np.asarray(raw_dataset)
+        print(slice_0.shape)
+        _, rcs = patch_finding.patch_opt(slice_0)
+        iupper = rcs[0] # row start
+        ileft = rcs[1]  # column start
+
+
     iright = ileft + pw
     ilower = iupper + ph
     cropped_stack = np.zeros((raw_dataset.n_frames, ph, pw)) # pay attention to the stack size!
@@ -77,7 +92,11 @@ def shift_stack(stack, shift_coord):
 
     return stack
 
+
 def shift_stack_onfile(fpath, shift_coord, new_path = None, partial = False, srange = (0, 500)):
+    '''
+    Open a stack, take the shifted coordinates and correct the stack on file.
+    '''
     if partial:
         print("Only save part of the stack.")
         with tf.TiffFile(fpath) as tif:
@@ -86,6 +105,7 @@ def shift_stack_onfile(fpath, shift_coord, new_path = None, partial = False, sra
             tif.close()
     else:
         raw_img = tf.imread(fpath)
+
     n_slice, ny, nx = raw_img.shape
     for ii in range(n_slice):
         frame = raw_img[ii]
@@ -110,67 +130,17 @@ def noise_simulation(NZ, NY, NX, model = 'gauss', mean = 160, sig = 100):
     return noise_stack
 
 
-
-def stack_simulation(img, shift_list, noise_model = 'gauss', noise_level = 0.1):
-    '''
-    simulate a noisy, drifted image with raw image as the seed; if img is None, then simulate a stack with noise only.
-    '''
-    NZ = len(shift_list) + 1
-    sig_mean, sig_std = np.mean(img), np.std(img)
-    if img is None:
-        NY, NX = NR
-    else:
-        NY, NX = img.shape
-
-    stack = np.zeros((NZ, NY, NX))
-    stack[0] = img
-    for ii in range(1, NZ):
-         stack[ii] = interpolation.shift(img,shift_list[ii-1])
-
-    if noise_level > 0:
-        noise_stack = noise_simulation(NZ, NY, NX, noise_model, mean = noise_level*sig_mean, sig = sig_std*noise_level)
-        print(noise_stack.max())
-        stack = stack + noise_stack
-
-    return stack
-
-
-
 # ---------------------------------------Below is the main function for test.-----------------------------------
 
 def main():
+    # OK the shift-on-site problem also got solved.
     folder_list = glob.glob(global_datapath_ubn+"*slice.tif")
-    for data_path in folder_list:
-        print(data_path)
-        sample_stack= tf.imread(data_path).astype('float64')
-        noise_slice = np.random.poisson(size = (512,512), lam = 0.5)
-        noise_slice = noise_slice**2
-        ft_raw = correlation.fft_image(sample_stack[1])
-        sample_hanning = stack_hanning(sample_stack)
-        ft_han = correlation.fft_image(sample_hanning[1])
-        ft_noise= correlation.fft_image(noise_slice)
+    print(folder_list[0])
+    cropped_stack = stack_crop(folder_list[0], seek_mode = 'opt')
+    shift_c = correlation.cross_corr_stack_self(cropped_stack)
+    print(shift_c)
+    shift_stack_onfile(folder_list[0], shift_c, partial = True, srange = (0,3))
 
-        fig_raw = brain_navigation.slices_compare(sample_stack[1], np.log(ft_raw), 'Raw image', 'Fourier Transform')
-        fig_han = brain_navigation.slices_compare(sample_hanning[1], np.log(ft_han), 'Hanning-filtered image', 'Fourier Transform')
-        fig_noise = brain_navigation.slices_compare(noise_slice, np.log(ft_noise), 'Simulated noise', 'Fourier Transform')
-        fig_raw.savefig('raw_fft')
-        fig_han.savefig('han_fft')
-        fig_noise.savefig('noise_fft')
-
-
-
-        shift_coord = correlation.cross_corr_stack_self(sample_stack, pivot_slice = 0)
-        np.savetxt(global_datapath_ubn + 'drift_raw.txt', shift_coord, delimiter = '\t', fmt = '%d')
-        vraw = shift_coord.var(axis = 0 )
-        mraw = np.abs(shift_coord).sum(axis = 0)
-        shift_coord = correlation.cross_corr_stack_self(sample_hanning, pivot_slice = 0)
-        vhan = shift_coord.var(axis = 0 )
-        mhan = np.abs(shift_coord).sum(axis = 0)
-        np.savetxt(global_datapath_ubn + 'drift_han.txt', shift_coord, delimiter = '\t', fmt = '%d')
-        print("mean of raw:", mraw)
-        print("mean of han-filtered:", mhan)
-        #print(shift_coord)
-        #shift_stack_onfile(data_path, shift_coord, new_path = global_datapath_win+ '/test.tif')
 
 if __name__ == '__main__':
     main()
