8889a1d40cc0f241c79e2421669433dafac8e3c1
danustc <xdlaway@gmail.com>
2018-12-04 10:32:18 -0800

    1204_Rock-0

--- Zbrain_prep.py
+++ Zbrain_prep.py
@@ -1,20 +1,20 @@
 import sys
+package_path_win ='/c/Users/Admin/Documents/GitHub/Image_toolbox/' # for windows
+sys.path.append(package_path_win)
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 import glob
-import src.shared_funcs.tifffunc as tf
-import src.preprocessing.coord_transform as coord_trans
+import tifffile as tf
+import src.registration.coord_transform as coord_trans
 import src.preprocessing.stack_operations as stack_operations
 import src.registration.maskdb_parsing as maskdb
 from src.shared_funcs.numeric_funcs import circ_mask_patch, spheri_mask_patch
 #---------------Some global variables--------------------
 
-package_path_win ='/c/Users/Admin/Documents/GitHub/Image_toolbox/src/' # for windows
 global_datapath_ubn = '/home/sillycat/Programming/Python/data_test/'
-global_datapath_win = 'D:\\/Data/2018-07-19/'
-sys.path.append('/home/sillycat/Programming/Python/Image_toolbox/')
-#sys.path.append(package_path_win)
+global_datapath_win = 'D:\\/Data/2018-12-03/'
+#sys.path.append('/home/sillycat/Programming/Python/Image_toolbox/')
 portable_datapath = '/media/sillycat/DanData/'
 regist_path = '/home/sillycat/Programming/Python/Image_toolbox/cmtkRegistration/'
 pxl_img = [0.295, 0.295, 1.00]
@@ -24,7 +24,7 @@ def scr_padding():
     '''
     pad all the ZD stacks with zeros and resave
     '''
-    ZD_list = glob.glob(global_datapath_win+'Jul19*/*ZD*.tif') # This is for ubuntu
+    ZD_list = glob.glob(global_datapath_win+'Dec*/*ZD*.tif') # This is for ubuntu
     #ZD_list = glob.glob(global_datapath+'Jun*/*ZD*.tif') # This is for windows 
     print(ZD_list)
     for ZD_file in ZD_list:
@@ -32,11 +32,11 @@ def scr_padding():
         dirname = os.path.dirname(ZD_file)
         base_dir = ''.join(os.path.basename(dirname).split('_'))
         print(base_dir)
-        ZD_stack = tf.read_tiff(ZD_file)
+        ZD_stack = tf.imread(ZD_file)
         zz, zy, zx = np.where(ZD_stack==0)
         ZD_stack[zz,zy,zx] = np.abs(np.random.randn(len(zz))*15)
         #TH_stack = stack_operations.stack_global_thresholding(ZD_stack, nsig = th_sig)
-        tf.write_tiff(ZD_stack, global_datapath_win+base_dir+'.tif')
+        tf.imsave(global_datapath_win+base_dir+'.tif', ZD_stack.astype('uint16'))
 
 
 def scr_fromref():
@@ -44,7 +44,7 @@ def scr_fromref():
     #data_path = '/home/sillycat/Programming/Python/cmtkRegistration/'
     ref_path = 'rfp_temp.tif'
 
-    ref_stack = tf.read_tiff(global_datapath+ref_path)
+    ref_stack = tf.imread(global_datapath+ref_path)
     rm_yaxis = coord_trans.rotmat_yaxis(36.0)
     pxl_img = [0.295, 0.295, 1.00]
     pxl_lab = [0.798, 0.798, 2.00]
@@ -54,7 +54,7 @@ def scr_fromref():
     sample_range_MB = np.array([1050, 1450, 120]) # the sample range is ordered reversely w.r.t the stack shape, i.e., x--y--z.
     ref_range = np.array([138, 621, 1406])
     sample_value = coord_trans.sample_from_refstack(ref_stack, sample_range_MB, pxl_lab, pxl_img, rm_yaxis, origin_shift_MB)
-    tf.write_tiff(sample_value, global_datapath + 'RFP_midbrain.tif' )
+    tf.imwrite(global_datapath + 'RFP_midbrain.tif', sample_value )
     print("done!")
 
 
@@ -75,7 +75,8 @@ def scr_tempcrop():
         ref_stack = np.array(hf[temp_key])
         simp_key = '_'.join(temp_key.split('_')[:-1])
         sample_value = sample_from_refstack(ref_stack, sample_range, pxl_lab, pxl_img, rm_yaxis, origin_shift)
-        tf.write_tiff(sample_value, data_path + simp_key + '.tif')
+        tf.imsave(data_path + simp_key + '.tif', sample_value.astype('uint16'))
+
     hf.close()
     print("done!")
 
