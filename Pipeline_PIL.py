f73c650432f7256fcd9d4775f5fc8669ce47b3b9
danustc <xdlaway@gmail.com>
2018-12-09 14:06:01 -0800

    1209_Rock-0

--- Pipeline_PIL.py
+++ Pipeline_PIL.py
@@ -12,12 +12,12 @@ import sys
 import glob
 import numpy as np
 sys.path.append(package_path_win)
-import src.shared_funcs.tifffunc as tifffunc
 import matplotlib.pyplot as plt
+from PIL import Image
 from src.preprocessing.segmentation import *
 #from src.preprocessing.drift_correction import DC_pipeline
 
-data_rootpath_win ='D:/Data/2018-08-02/Aug02_2018_B1\\'
+data_rootpath_win ='X:/Zebrafish_ispim/2018-09-24/Sep24_2018_B2/\\'
 data_rootpath_yst ='Z:/Dan/Data_Rock/Sep24_2018_A2/\\'
 data_rootpath_portable ='/media/sillycat/DanData/Jul19_2017_A2/\\'
 #folder_list = glob.glob(data_rootpath+"/B3_TS\\")
@@ -56,7 +56,6 @@ class pipeline_tstacks(object):
                             3 --- extracted
                             -1 --- error
             '''
-            self.CE_dpt = Cell_extract(im_stack = None, diam = cdiam) # the default diameter of the blob: 9
         return
 
     def load_file(self, nfile, size_th = 600, verbose = True):
@@ -65,25 +64,25 @@ class pipeline_tstacks(object):
         size_th: threshold of the data file. Unit: MB.
         '''
         fname = self.raw_list[nfile]; print(fname)
-        stack_size, stack_shape, self.tif_handle = tifffunc.tiff_describe(fname, handle_open = True)
+        self.pim = Image.open(fname) # open the image
+        n_frames = self.pim.n_frames
+        w,h = self.pim.size
+        stack_size= w*h*n_frames/(600*1024)
+
         if(stack_size > size_th):
             self.stepload = True
             n_groups = int(stack_size/size_th)+1
-            nslices = stack_shape[0]
-            ss_slices = int(nslices/n_groups)+1 # number of slices to import everytime
+            ss_slices = int(n_frames/n_groups)+1 # number of slices to import everytime
             stack_cut = np.zeros(n_groups + 1)
-            stack_cut[:n_groups] = np.arange(nslices, step = ss_slices)
-            stack_cut[-1] = nslices # the cutting-off positions
-            self.stack_cut = stack_cut
+            stack_cut[:n_groups] = np.arange(n_frames, step = ss_slices)
+            stack_cut[-1] = n_frames# the cutting-off positions
+            self.stack_cut = stack_cut.astype('uint16')
             self.n_groups = n_groups
             if verbose:
                 print("number of group:", n_groups)
                 print("Cutting-off places:", stack_cut)
         else:
             self.stepload = False
-
-
-        self.stack_shape = stack_shape
         self.current_file = nfile
 
 
@@ -92,7 +91,12 @@ class pipeline_tstacks(object):
         Read nsamples from the filehandle
         nsamples is an array or a list
         '''
-        sample_stack = self.tif_handle.asarray()[nsamples] # this step is pretty time consuming
+        sample_stack = []
+        for ns in nsamples:
+            self.pim.seek(ns)
+            sample_stack.append(np.array(self.pim)) # this step is pretty time consuming)
+        
+        sample_stack = np.array(sample_stack)
 
         blobs_sample = stack_blobs(sample_stack, self.cdiam, sig = 5)
         self.cblobs = stack_redundreduct(blobs_sample, th = 5) # redundancy removed substack, saves the y,x coordinates of the extracted blobs
@@ -109,14 +113,21 @@ class pipeline_tstacks(object):
         # stepload or one-time load?
         if(self.stepload):
             signal_series = [] # create an empty list, which should be merged later
-            si = self.stack_cut[0]
-            for n_step in range(self.n_groups):
+            
+            for n_step in range(3,self.n_groups):
+                si = self.stack_cut[n_step]
                 sf = self.stack_cut[n_step+1]
-                substack = self.tif_handle.asarray()[np.arange(si, sf).astype('uint16')] # this is a pretty risky approach, hopefully it can work! @_@
-                self.CE_dpt.stack_reload(substack, refill = True)
+                substack = []
+                print(si,sf)
+                for ii in range(si, sf):
+                    self.pim.seek(ii)
+                    substack.append(np.array(self.pim))
+    
+                #print(substack[0].shape)
+                substack = np.array(substack)
+                #print(substack.shape)
                 sub_time_series = stack_signal_propagate(substack, cblobs) # return
                 signal_series.append(sub_time_series)
-                si = sf
                 if verbose:
                     print("Processed step ", n_step)
             # now, let's concatenate the substacks in the list and compile it into a new dataset 
@@ -126,14 +137,11 @@ class pipeline_tstacks(object):
         else: # one-time load
             raw_stack = self.tif_handle.asarray()
             sample_frame = raw_stack[0]
-            self.CE_dpt.stack_reload(raw_stack)
             ts_signal = stack_signal_propagate(raw_stack, cblobs)
 
         self.ts_dataset = position_signal_compile(cblobs, ts_signal)
         np.savez(fname_stem, **self.ts_dataset)
-        self.tif_handle.filehandle.close()
-        self.tif_handle.close() # close the tif handle 
-        self.tif_handle = None
+        self.pim.close()
         self.process_log[self.current_file] = 3
 
         sample_frame = frame_deblur(sample_frame, sig = 5, Nit = 15)
@@ -169,8 +177,8 @@ class pipeline_tstacks(object):
 
 # -----------------------The main test function -----------------------
 def main():
-    folder_list = glob.glob(data_rootpath_yst+"/A2_TS/\\")
-    #folder_list = glob.glob(data_rootpath_win+"/B2_TS/\\")
+    #folder_list = glob.glob(data_rootpath_yst+"/A2_TS/\\")
+    folder_list = glob.glob(data_rootpath_win+"/B2_TS/\\")
     for data_path in folder_list:
         print(data_path)
         pt = pipeline_tstacks(data_path, fname_flags = 'rg')
